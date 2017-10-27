8a067a5654aedb66907ab05b1da9c95f0201881b
xiacaishuang <751405162@qq.com>
2017-10-27 10:49:26 +0800

    分页

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display=('title','slug','author','publish','status')
-    list_filter = ('status', 'created', 'publish', 'author')
-    search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
+    list_display=('title','slug','author','publish','status')#定义要显示的栏目
+    list_filter = ('status', 'created', 'publish', 'author')#定义多个查询功能
+    search_fields = ('title', 'body')#定义一个搜索框，从title和body的内容中进行搜索
+    prepopulated_fields = {'slug': ('title',)}#将title中的内容预填充到slug中
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from blog import views
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),#post_list
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
